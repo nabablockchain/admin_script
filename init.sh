f0fbc06ecf9b96961657c18525c05934e663437a
lege <legeishere@163.com>
2016-09-21 01:54:33 +0000

    some script

---
+++ init.sh
@@ -0,0 +1,7 @@
+apt-get install git -y
+apt-get install sqlite3 libsqlite3-dev
+apt-get install python-dev
+apt-get install python-pip
+pip install --upgrade pip
+pip install pycrypto
+apt-get install npm
